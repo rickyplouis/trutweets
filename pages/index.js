df3059e9c86d13e7ee64c3e8ff44053e055ed098
Ricardo <ricardoplouis@gmail.com>
2018-08-31 12:13:12 -0500

    Client side tweet validation

--- pages/index.js
+++ pages/index.js
@@ -40,11 +40,26 @@ class Index extends Component {
       user,
       token,
       decodedToken,
+      currentTweet: '',
     };
+    this.handleTweet = this.handleTweet.bind(this);
+    this.handleProp = this.handleProp.bind(this);
+  }
+
+  handleProp(val, name) {
+    this.setState(prevState => ({
+      ...prevState,
+      [name]: val,
+    }));
+  }
+
+
+  handleTweet(evt) {
+    this.handleProp(evt.target.value, 'currentTweet');
   }
 
   render() {
-    const { user, token, decodedToken } = this.state;
+    const { user, token, decodedToken, currentTweet } = this.state;
     return (
       <Container activePath={['1']}>
         <Row>
@@ -52,11 +67,12 @@ class Index extends Component {
             <Avatar icon="user" style={{ background: 'darkblue' }} />
           </Col>
           <Col span={22}>
-            <TextArea rows={3} placeholder="What's on your mind?" />
+            <TextArea rows={3} placeholder="What's on your mind?" onChange={this.handleTweet} />
           </Col>
         </Row>
         <span style={{ textAlign: '-webkit-right', display: '-webkit-box', paddingTop: '10px' }}>
-          <Button>Submit</Button>
+          {currentTweet.length > 280 && <span style={{ color: 'red' }}>TruTweets must be less than 280 chars </span>}
+          <Button disabled={currentTweet.length === 0 || currentTweet.length > 280}>Submit</Button>
         </span>
         <Row style={{ paddingTop: '10px' }}>
           <Col span={24}>
@@ -68,12 +84,12 @@ class Index extends Component {
                 Insert some provocative tweet here that the user can vote up or down
               </h3>
               <Meta
+                style={{ paddingLeft: '10px' }}
                 avatar={<Avatar icon="user" style={{ background: 'darkblue' }} />}
                 description="By some user"
               />
               <span>
                 <Progress percent={50} status="active" showInfo={false} style={{ padding: '10px' }} />
-                Active for: 50 seconds
               </span>
             </Card>
           </Col>
