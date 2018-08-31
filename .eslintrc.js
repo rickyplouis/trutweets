6ac29c731f69c53b20c120089a7fa15772025fcb
Ricardo <ricardoplouis@gmail.com>
2018-08-30 21:06:09 -0500

    Added linter

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "extends": "airbnb",
+    "plugins": [
+        "react",
+        "jsx-a11y",
+        "import"
+    ],
+    "rules": {
+      "react/jsx-filename-extension": [0],
+      camelcase: 'off',
+      "react/no-danger": 'off',
+      "jsx-a11y/anchor-has-content": "off"
+    }
+};
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
-import React from 'react'
-import Link from 'next/link'
+import React from 'react';
+import Link from 'next/link';
 
 export default () => (
-  <ul>
-    <li><Link href='/a' as='/a'><a>a</a></Link></li>
-    <li><Link href='/b' as='/b'><a>b</a></Link></li>
-  </ul>
-)
+  <div>
+    <li>
+      <Link href="/a" as="/a"><a href="/a">a</a></Link>
+    </li>
+    <li><Link href="/b" as="/b"><a href="/b">b</a></Link></li>
+  </div>
+);
