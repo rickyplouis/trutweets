940250da623dc1c131f5caa0ababe11236f541b3
Ricardo <ricardoplouis@gmail.com>
2018-08-30 21:19:08 -0500

    Integrated antd

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import Link from 'next/link';
+import Head from 'next/head';
+import { Progress } from 'antd';
 
 export default () => (
   <div>
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta charSet="utf-8" />
+      <link rel="stylesheet" href="/_next/static/style.css" />
+    </Head>
+    <Progress percent={50} status="active" />
     <li>
       <Link href="/a" as="/a"><a href="/a">a</a></Link>
     </li>
